1670115cc8f199aa267502065b2c0b2b6b5994e8
eimarramos <eimarramos.sacari@gmail.com>
2024-05-31 17:52:40 +0200

    feat: Estructura de la carta pokemon

---
+++ src/pages/Home/components/ListItem/ListItem.tsx
@@ -0,0 +1,28 @@
+
+
+export const ListItemComponent = () => (
+    <article>
+        <header>
+            <h3>Bulbasaur</h3>
+            <p>#001</p>
+        </header>
+        <section>
+            <img src="" alt="" />
+            <div>
+                <p>Grass</p>
+                <p>Poison</p>
+            </div>
+            <div>
+                <p> 6.9 Kg</p>
+                <p> 0.7 m</p>
+            </div>
+            <div>
+                <p>HP</p>
+                <p>045</p>
+                <div>
+                    <span></span>
+                </div>
+            </div>
+        </section>     
+    </article>
+)
