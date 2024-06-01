74fc3364480481614d0861ae07587a61fe5f365f
eimarramos <eimarramos.sacari@gmail.com>
2024-06-01 13:30:39 +0200

    feat: Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { NavbarComponent } from './components/Navbar/Navbar'
 import { HomeComponent } from './pages/Home/Home'
+import { FooterComponent } from './components/Footer/Footer'
 
 function App() {
   return (
-    <>
+    <main>
       <NavbarComponent />
       <HomeComponent />
-    </>
+      <FooterComponent />
+    </main>
   )
 }
 
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,8 @@
+import style from './Footer.module.css'
 
 
 export const FooterComponent = () => (
-    <footer >
-   
+    <footer className={style.footer}>
+        <p className={style.footer__text}>©2024 Pokémon. ©1995 - 2024 Nintendo/Creatures Inc./GAME FREAK inc. TM, ®Nintendo.</p>
     </footer>
   )
