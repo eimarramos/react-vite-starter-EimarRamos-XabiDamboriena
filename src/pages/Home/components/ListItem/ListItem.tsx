2ac250eeba6f6863d18b4c1c025778e9209ebd7e
eimarramos <eimarramos.sacari@gmail.com>
2024-06-01 10:55:53 +0200

    feat: pills css

--- src/pages/Home/components/ListItem/ListItem.tsx
+++ src/pages/Home/components/ListItem/ListItem.tsx
@@ -8,10 +8,18 @@ export const ListItemComponent = () => (
             <p className={style.card_header_number}>#001</p>
         </header>
         <section className={style.card_body}>
-            <img className={style.card_body__photo} src={bulbasaur} alt="" />
+            <div className={style.card_body__image_container}>
+                <img className={style.card_body__image} src={bulbasaur} alt="" />
+            </div>     
             <div className={style.card_body__types}>
-                <p>Grass</p>
-                <p>Poison</p>
+                <div className={`${style.card_body_pills} ${style['card_body_pills--grass']}`}>
+                    <img src={bulbasaur} alt="" className={style.card_body__pills_icon}/>
+                    <p>Grass</p>
+                </div>
+                <div className={`${style.card_body_pills} ${style['card_body_pills--poison']}`}>
+                    <img src={bulbasaur}  alt="" className={style.card_body__pills_icon}/>
+                    <p>Poison</p>
+                </div>
             </div>
             <div className={style.card_body__measures}>
                 <p> 6.9 Kg</p>
