63d7e58a6e8748e036eb56358da34a4c40974c96
eimarramos <eimarramos.sacari@gmail.com>
2024-06-10 23:49:13 +0200

    feat: Formatear numeros en los stats

--- src/pages/Home/components/Stat/Stat.tsx
+++ src/pages/Home/components/Stat/Stat.tsx
@@ -1,3 +1,4 @@
+import { formatNumber } from '../../../../utils/NumberFormater/NumberFormater'
 import style from './Stat.module.css'
 
 type StatProps = {
@@ -10,7 +11,7 @@ export const StatComponent: React.FC<StatProps> = ({ name, value }) => {
     <div className={style.stat_container}>
       <div className={style.stat_name_value_container}>
         <p className={style.stat_container__stat_name}>{name}</p>
-        <p className={style.stat_container__stat_value}>{value}</p>
+        <p className={style.stat_container__stat_value}>{formatNumber(value)}</p>
       </div>
       <progress
         max="255"
