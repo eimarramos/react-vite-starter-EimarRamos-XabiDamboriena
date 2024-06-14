77f3498cf1159544a564abbe73ea88ce14d46bfd
eimarramos <eimarramos.sacari@gmail.com>
2024-06-14 18:13:53 +0200

    feat: progress bar

--- src/pages/Home/components/Stat/Stat.tsx
+++ src/pages/Home/components/Stat/Stat.tsx
@@ -11,13 +11,11 @@ export const StatComponent: React.FC<StatProps> = ({ name, value }) => {
     <div className={style.stat_container}>
       <div className={style.stat_name_value_container}>
         <p className={style.stat_container__stat_name}>{name}</p>
-        <p className={style.stat_container__stat_value}>{formatNumber(value)}</p>
+        <p className={style.stat_container__stat_value}>
+          {formatNumber(value)}
+        </p>
       </div>
-      <progress
-        max="255"
-        value={value}
-        className={style.bar_progress}
-      ></progress>
+      <progress max="255" value={value} className={style.bar_progress} />
     </div>
   )
 }
