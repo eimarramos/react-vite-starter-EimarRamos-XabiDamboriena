70bebe69570aadf76576b917f713cda35f26d566
xabid98 <xabidamboriena@gmail.com>
2024-06-22 12:34:26 +0200

    feat: implementar getPokemonDetails en pokemonService

--- src/domain/pokemon/services/pokemonService/pokemonService.ts
+++ src/domain/pokemon/services/pokemonService/pokemonService.ts
@@ -2,20 +2,9 @@ import { BASE_URL } from '../../../../config/config'
 import { PaginationResponse } from '../../../../types/PaginationResponse'
 import { PokeApiDTO } from '../../../../infrastructure/api/pokeapi/types/PokeApiDTO'
 import { mapPokemon } from '../../../../infrastructure/api/pokeapi/PokemonMapper/PokemonMapper'
+import { getPokemonDetails } from '../../../../infrastructure/api/pokeapi/getPokemon/getPokemonDetail'
 
 export const pokemonService = async () => {
-  const data = await fetch(`${BASE_URL}?limit=${150}&offset=0`)
-  const paginationResponse: PaginationResponse = await data.json()
-
-  const promises = paginationResponse.results.map(async pokemonDTO => {
-    const response = await fetch(`${BASE_URL}${pokemonDTO.name}`)
-    const pokeApiResponse: PokeApiDTO = await response.json()
-    const pokemonMapped = mapPokemon(pokeApiResponse)
-
-    return pokemonMapped
-  })
-
-  const pokemonArray = await Promise.all(promises)
-
-  return pokemonArray
+  const pokemonsDetails = getPokemonDetails()
+  return pokemonsDetails
 }
