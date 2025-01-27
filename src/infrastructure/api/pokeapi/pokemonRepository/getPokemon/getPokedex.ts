6b8f26071c781ab5639c490e983d99e38f8c7fe8
Eimar Ramos <91671632+eimarramos@users.noreply.github.com>
2025-01-27 19:52:20 +0100

    Actualizar getPokedex.ts

--- src/infrastructure/api/pokeapi/pokemonRepository/getPokemon/getPokedex.ts
+++ src/infrastructure/api/pokeapi/pokemonRepository/getPokemon/getPokedex.ts
@@ -2,7 +2,7 @@ import { BASE_URL } from '../../../../../config/config'
 import { PaginationResponse } from '../../types/PaginationResponse'
 
 export const getPokedex = async () => {
-  const data = await fetch(`${BASE_URL}?limit=${150}&offset=0`)
+  const data = await fetch(`${BASE_URL}`)
   const paginationResponse: PaginationResponse = await data.json()
 
   return paginationResponse
